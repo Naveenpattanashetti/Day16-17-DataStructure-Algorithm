b72570f78b893ef2cd1eb7528fccf1e800b6a843
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-19 13:46:57 +0530

    added:welcome Messgae to User

---
+++ src/main/java/com/bridgelabz/DataStructure_Algorithm/RecursivePermutation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.DataStructure_Algorithm;
+
+import java.util.Scanner;
+
+public class RecursivePermutation {
+	public static void main(String[] args) {
+		System.out.println("welcome to user");
+	}
+}
