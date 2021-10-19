04671a297b85f7c37544fd7c1373c852372b802d
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-19 14:50:28 +0530

    Added : Program for the Data Structure and Algorithm

---
+++ src/main/java/com/bridgelabz/DataStructure_Algorithm/BubbleSort.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.DataStructure_Algorithm;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		BubbleSort sort = new BubbleSort();
+		int arr[] = { 60, 35, 25, 50, 12, 11, 80 };
+		sort.bubbleSort(arr);
+		System.out.println("Sorted array : ");
+		sort.printArray(arr);
+	}
+
+	void bubbleSort(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++)
+			for (int j = 0; j < n - i - 1; j++)
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+	}
+
+	void printArray(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+}
